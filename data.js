47423900583e9c412f60ee7a38679762256a42b7
unknown <enesbc46@gmail.com>
2020-09-07 13:51:54 +0300

    iyi oldu gibi

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    song: "Here's to you",
+    channel: "enesbugrac",
+    likes: 110,
+    description: "İyi gün.",
+    shares: 150,
+    messages: 129,
+    url:
+      "https://instagram.fada2-2.fna.fbcdn.net/v/t50.2886-16/116318243_114096520231264_5248633621650744716_n.mp4?_nc_ht=instagram.fada2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=4pYK6OLRZOUAX8RkH3t&oe=5F564E06&oh=22f2bd3da40bd505028cb40e99d42fd0",
+  },
+  {
+    song: "Here's to you",
+    channel: "enesbugrac",
+    likes: 110,
+    description: "İyi gün.",
+    shares: 150,
+    messages: 129,
+    url:
+      "https://instagram.fada2-2.fna.fbcdn.net/v/t50.2886-16/116318243_114096520231264_5248633621650744716_n.mp4?_nc_ht=instagram.fada2-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=4pYK6OLRZOUAX8RkH3t&oe=5F564E06&oh=22f2bd3da40bd505028cb40e99d42fd0",
+  },
+];
+export default data;
---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import data from "./data.js";
+import Videos from "./dbModel.js";
+//app config
+const app = express();
+const port = 9000;
+
+//middlewares
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Header", "*");
+  next();
+});
+//DB config
+const connection_url =
+  "mongodb+srv://enesbugrac:enesbugrac@cluster0.2y5hv.mongodb.net/tiktok?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+//api endpoints
+
+app.get("/", (req, res) => res.status(200).send("25"));
+
+app.get("/v1/posts", (req, res) => res.status(200).send(data));
+
+app.get("/v2/posts", (req, res) => {
+  Videos.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/v2/posts", (req, res) => {
+  const dbVideos = req.body;
+
+  Videos.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//listen
+app.listen(port, () => console.log("listening on localhost" + port));
